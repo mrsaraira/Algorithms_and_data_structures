2ed9f74b48ed6d5764d96aae441ef12dbdbf1896
Takhsin <takhsin.sarayra@hys-enterprise.com>
2019-09-08 19:06:21 +0300

    init commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+M
+src/main/java/Classes/UF.java,c\d\cd42c8636ae40ea32f798d946231e3a0897fd7d3
---
+++ src/main/java/Classes/QuickUnionUFWeighted.java
@@ -0,0 +1,50 @@
+package Classes;
+
+import java.util.Arrays;
+
+public class QuickUnionUFWeighted {
+    private int[] id;
+    private int[] sz;
+
+    public QuickUnionUFWeighted(int n) {
+        id = new int[n];
+        sz = new int[n];
+        for (int i = 0; i < n; i++) {
+            id[i] = i;
+            sz[i] = 1;
+        }
+    }
+
+    public QuickUnionUFWeighted() {
+    }
+
+    private int root(int i) {
+        while (i != id[i])
+            i = id[i];
+        return i;
+    }
+
+    public void union(int p, int q) {
+        int i = root(p);
+        int j = root(q);
+        if (i == j) return;
+        if (sz[i] < sz[j]) {
+            id[i] = j;
+            sz[j] += sz[i];
+        } else {
+            id[j] = i;
+            sz[i] += sz[j];
+        }
+    }
+
+    public boolean connected(int p, int q) {
+        return root(p) == root(q);
+    }
+
+    @Override
+    public String toString() {
+        return "QuickUnionUF{" +
+                "id=" + Arrays.toString(id) +
+                '}';
+    }
+}
